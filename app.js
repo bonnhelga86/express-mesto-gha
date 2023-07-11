3fa2aba3fe9f4f92bdc7d930330d96cfff7c803c
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-07-11 20:48:55 +0400

    feat: Настроить проект, добавить модели card и user

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log('we are here');
+  console.log('we are here again');
+});
