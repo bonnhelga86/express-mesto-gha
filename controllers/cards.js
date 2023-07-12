b680343efcfe4924fb7c74b7464895f02bb08b89
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-07-12 14:25:18 +0400

    feat: Настроить формат ответа сервера

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,33 +3,38 @@ const { errorCatch } = require('../utils/error');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
+    .populate('owner')
     .then((cards) => res.send({ data: cards }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const userId = req.user._id;
+  const owner = req.user._id;
 
-  Card.create({ name, link, userId })
+  Card.create({ name, link, owner })
+    .populate('owner')
     .then((card) => res.send({ data: card }))
-    .catch((error) => errorCatch(error, res));
+    .catch((error) => res.send({ message: error }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
+    .populate('owner')
     .then((card) => res.send({ data: card }))
     .catch((error) => errorCatch(error, res));
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .populate('owner')
     .then((card) => res.send({ data: card }))
     .catch((error) => errorCatch(error, res));
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .populate('owner')
     .then((card) => res.send({ data: card }))
     .catch((error) => errorCatch(error, res));
 };
