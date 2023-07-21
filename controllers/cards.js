cc91b15a67135336c347f32e30814b43c327a62e
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-07-21 21:02:19 +0400

    feat:  Добавить обработки ошибок в контроллер card, переписать его с async await

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,51 +1,73 @@
-const mongoose = require('mongoose');
 const Card = require('../models/card');
-const { errorCatch, errorsCode, getResponseData } = require('../utils/helpers');
+const { NotFoundError } = require('../errors/not-found-error');
+const { AuthorizationError } = require('../errors/authorization-error');
 
-module.exports.getCards = (req, res) => {
-  Card.find({})
-    .populate('owner')
-    .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(errorsCode.invalidServer.key)
-      .send({ message: errorsCode.invalidServer.message }));
+module.exports.getCards = async (req, res, next) => {
+  let cards;
+  try {
+    cards = await Card.find({}).populate('owner');
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.send({ data: cards });
 };
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = async (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user;
-
-  getResponseData.call(Card.create({ name, link, owner }), res);
+  let card;
+  try {
+    card = await Card.create({ name, link, owner });
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.send({ data: card });
 };
 
-module.exports.deleteCard = (req, res) => {
-  Card.findById(req.params.cardId).orFail()
-    .then((card) => {
-      if (!card.owner._id.equals(req.user._id)) {
-        errorCatch(new mongoose.Error.AuthorizationError(), res);
-      }
-      getResponseData.call(Card.findByIdAndRemove(req.params.cardId).orFail()
-        .populate('owner'), res);
-    });
+module.exports.deleteCard = async (req, res, next) => {
+  let card;
+  try {
+    card = await Card.findById(req.params.cardId).populate('owner');
+    if (!card) throw new NotFoundError('Карточка не найдена');
+    if (!card.owner._id.equals(req.user._id)) throw new AuthorizationError('Вы можете удалять только свои карточки');
+    await Card.findByIdAndRemove(req.params.cardId);
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.send({ data: card });
 };
 
-module.exports.likeCard = (req, res) => {
-  getResponseData
-    .call(
-      Card
-        .findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-        .orFail()
-        .populate('owner'),
-      res,
-    );
+module.exports.likeCard = async (req, res, next) => {
+  let card;
+  try {
+    card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    ).populate('owner');
+    if (!card) throw new NotFoundError('Карточка не найдена');
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.send({ data: card });
 };
 
-module.exports.dislikeCard = (req, res) => {
-  getResponseData
-    .call(
-      Card
-        .findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-        .orFail()
-        .populate('owner'),
-      res,
-    );
+module.exports.dislikeCard = async (req, res, next) => {
+  let card;
+  try {
+    card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    ).populate('owner');
+    if (!card) throw new NotFoundError('Карточка не найдена');
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.send({ data: card });
 };
